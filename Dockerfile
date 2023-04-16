ed24395ac462aceb1bebdc7d680c850f0e7c4b0f
changyou <changyou0730@gmail.com>
2023-04-16 16:55:16 +0800

    alter notion_page_id

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-ARG NOTION_PAGE_ID = 8c52c320248441e38501b5e353cfe162
+ARG NOTION_PAGE_ID =
 # Install dependencies only when needed
 FROM node:14-alpine AS deps
 # Check https://github.com/nodejs/docker-node/tree/b4117f9333da4138b03a546ec926ef50a31506c3#nodealpine to understand why libc6-compat might be needed.
--- blog.config.js
+++ blog.config.js
@@ -23,7 +23,7 @@ const BLOG = {
     keywords: ['Blog', 'Website', 'Notion'],
     googleSiteVerification: '' // Remove the value or replace it with your own google site verification code
   },
-  notionPageId: process.env.NOTION_PAGE_ID, // DO NOT CHANGE THIS！！！
+  notionPageId: '8c52c320248441e38501b5e353cfe162',// process.env.NOTION_PAGE_ID, // DO NOT CHANGE THIS！！！
   notionAccessToken: process.env.NOTION_ACCESS_TOKEN, // Useful if you prefer not to make your database public
   analytics: {
     provider: 'ga', // Currently we support Google Analytics and Ackee, please fill with 'ga' or 'ackee', leave it empty to disable it.
